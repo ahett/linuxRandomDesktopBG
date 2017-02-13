f136be83731a9cba6b37f10f58996f899837717d
Adrian Hett <adrian@hett.cc>
2017-02-13 23:07:26 +0000

    finalized

--- rndBG.sh
+++ rndBG.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
+# set PATHs
 WPPTH="/home/adrian/Bilder/Wallpaper/"
-
-#get number of wallpapers in folder
-MAX=$(ls $WPPTH -F |grep -v / | wc -l)
-echo "MAX: $MAX"
+FINAL="/home/adrian/Bilder/Wallpaper/__00final.jpg"
 
 #generate four ramndom numbers
 RNG1=$(shuf -i 1-$MAX -n 1)
@@ -17,37 +15,47 @@ FILE2=""
 FILE3=""
 FILE4=""
 
-echo "$RNG1, $RNG2, $RNG3, $RNG4"
-echo ""
-i=1
+#echo "$RNG1, $RNG2, $RNG3, $RNG4"
+
+i=0
 
 # iterate all files in wallpaper path
 for file in $WPPTH*
 do
-    if [ $i -eq $RNG1 ]
+#  echo "$file"
+  if [ $i -gt 0 ]       # skip __00final.jpg
+  then
+    if [ $i -eq $RNG1 ] # first file
     then
       FILE1=$file
     fi
-    if [ $i -eq $RNG2 ]
+    if [ $i -eq $RNG2 ] # second
     then
       FILE2=$file
     fi
-    if [ $i -eq $RNG3 ]
+    if [ $i -eq $RNG3 ] # third
     then
       FILE3=$file
     fi
-    if [ $i -eq $RNG4 ]
+    if [ $i -eq $RNG4 ] #fourth
     then
       FILE4=$file
     fi
-    ((i++))
+  fi
+  ((i++)) # increment
 done
-rm -f pics/*.*
 
-convert $FILE1 -resize 2560x1440! -quality 100 "pics/1.jpg"
-convert $FILE2 -resize 2560x1440! -quality 100 "pics/2.jpg"
-convert $FILE3 -resize 2560x1440! -quality 100 "pics/3.jpg"
-convert $FILE4 -resize 2560x1440! -quality 100 "pics/4.jpg"
-convert "pics/4.jpg" -crop 1920x1200+0+0 "pics/4.jpg"
-convert "pics/1.jpg" "pics/2.jpg" "pics/3.jpg" "pics/4.jpg" +append "pics/all.jpg"
-echo "1: $FILE1, 2: $FILE2, 3: $FILE3, 4: $FILE4"
+#make sure the siyes are right
+convert $FILE1 -resize 2560x1440! -quality 100 "/tmp/1.jpg"
+convert $FILE2 -resize 2560x1440! -quality 100 "/tmp/2.jpg"
+convert $FILE3 -resize 2560x1440! -quality 100 "/tmp/3.jpg"
+convert $FILE4 -resize 2560x1440! -quality 100 "/tmp/4.jpg"
+
+#crop for the smaller monitor
+convert "/tmp/4.jpg" -crop 1920x1200+0+0 "/tmp/4.jpg"
+
+# compose full image
+convert "/tmp/1.jpg" "/tmp/2.jpg" "/tmp/3.jpg" "/tmp/4.jpg" +append "$FINAL"
+
+# set wallpaper
+gsettings set org.gnome.desktop.background picture-uri file://"$FINAL"
